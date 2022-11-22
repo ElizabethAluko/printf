3ff3bcf310fa824e73fd8b094f4ce8e8cd0e7a24
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-22 07:11:05 -0800

    Add main.h file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _print_char(char c);
+int _print_str(char *s);
+int _print_int(int n);
+int (*_select_format(char c)(arg_list);
+
+
+#endif
