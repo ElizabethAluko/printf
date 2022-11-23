9dce51a3f7fba5460ad8b9460855e76082603640
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-22 19:56:16 -0800

    debugg

---
+++ test/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char g = 'A';
+	char str[] = "Ted";
+
+	_printf("Letter %c\n", g);
+	_printf("%s\n", str);
+	_printf("100%%\n");
+	return (0);
+}
